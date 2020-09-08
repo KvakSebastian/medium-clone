de8c7d53a4c1bc15393bf85b0b06551c2d9f74d4
Sebastian <sebi.kvak@gmail.com>
2020-09-08 19:47:36 +0300

    fix: css loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill', './src/js/index.js'],
+    entry: ['babel-polyfill', './src/js/index.js','./src/css/style.css'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
@@ -14,7 +15,14 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
-        })
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'articles.html',
+            template: './src/articles.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/style.css',
+          })
     ],
     module: {
         rules: [
@@ -24,7 +32,23 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
-            }
+            },
+            {test: /\.css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: '../',
+                },
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                },
+              },
+            ]
+          }
         ]
     }
 };

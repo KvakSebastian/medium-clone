d0e2a1e69d7b2f74aa29eacacc39f308c15d1335
Sebastian <sebi.kvak@gmail.com>
2020-09-13 21:14:00 +0300

    temp

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 // Global app controller
+import ArticleService from './services/articles-service.js'
+
+let service = new ArticleService();
+const res = service.getResource()
+.then();
+console.log(res);
---
+++ src/js/services/articles-service.js
@@ -0,0 +1,13 @@
+export default class ArticleService {
+
+    getResource = async () => {
+      const res = await fetch(`https://5f5bcf95044570001674d370.mockapi.io/medium-clone`);
+  
+      if (!res.ok) {
+        throw new Error(`Could not fetch ` +
+          `, received ${res.status}`)
+      }
+      return await res.json();
+    };
+  
+  }

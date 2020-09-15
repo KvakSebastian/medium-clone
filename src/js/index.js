f047d284736e0725699ec9c147e3f4707dea585d
Artem5063281 <5063281@gmail.com>
2020-09-15 13:05:58 +0300

    add auth functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 // Global app controller
 import ArticleService from './services/articles-service.js'
 
+let isLogged = false;
+
 let service = new ArticleService();
 const res = service.getResource()
     .then(localStorage.setItem('articles', JSON.stringify(res)));
@@ -19,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const BTNS = document.querySelectorAll(".getStarted");
     const SPAN = document.getElementsByClassName("closeModalWindow")[0];
     const OK = document.querySelector('#modalWindow form input[type="button"]');
+    const LOGIN = document.getElementById('logIn');
     const EMAIL = document.querySelector('#modalWindow form [name="email"]');
     const PASSWORD = document.querySelector('#modalWindow form [name="password"]');
 
@@ -32,17 +35,22 @@ document.addEventListener('DOMContentLoaded', function() {
         MODAL.style.display = "none";
     });
 
-    OK.addEventListener('click', function() {
+    OK.addEventListener('click', auth);
+    LOGIN.addEventListener('click', $e => MODAL.style.display = "block");
+
+    function auth() {
         const EMAIL_VALUE = EMAIL.value;
         const PASSWORD_VALUE = PASSWORD.value;
 
         if ((EMAIL_VALUE === ADMIN.email) && (PASSWORD_VALUE === ADMIN.password)) {
+            isLogged = true;
+            document.getElementById('logIn').classList.add('invisible');
+            document.getElementById('logOut').classList.remove('invisible');
             MODAL.style.display = "none";
             location.href = "/articles.html";
             return;
         }
 
-        alert('Email or password is wrong')
-    });
-
+        alert('Email or password is wrong');
+    }
 });

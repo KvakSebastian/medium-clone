1fcd19a21ab0d7a56d1722960bf9608a71f3cee2
Artem5063281 <5063281@gmail.com>
2020-09-17 12:54:20 +0300

    add comment functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,17 @@
 // Global app controller
 import Articles from './models/Articles.js'
-import {renderArticles} from './views/articlesView.js'
-import {addArticleToLS, otherArticles, renderArticle} from './views/articleView.js'
+import { renderArticles } from './views/articlesView.js'
+import { addArticleToLS, otherArticles, renderArticle } from './views/articleView.js'
 import { renderEditArticle } from './views/editArticleView.js';
-
 
 const service = new Articles();
 
-const init = async () =>{
-    if (localStorage.getItem('articles') === null){
-  await service.getAllArticles();
-}
-    if (document.location.pathname=== '/articles.html') {
-      renderArticles();
+const init = async() => {
+    if (localStorage.getItem('articles') === null) {
+        await service.getAllArticles();
+    }
+    if (document.location.pathname === '/articles.html') {
+        renderArticles();
     }
 }
 
@@ -26,84 +25,105 @@ class User {
 }
 
 const ADMIN = new User('admin', 'admin');
-if (document.location.pathname=== '/') {
+if (document.location.pathname === '/') {
 
-document.addEventListener('DOMContentLoaded', function() {
-    const MODAL = document.getElementById("modalWindow");
-    const BTNS = document.querySelectorAll(".getStarted");
-    const SPAN = document.getElementsByClassName("closeModalWindow")[0];
-    const OK = document.querySelector('#modalWindow form input[type="button"]');
-    const EMAIL = document.querySelector('#modalWindow form [name="email"]');
-    const PASSWORD = document.querySelector('#modalWindow form [name="password"]');
+    document.addEventListener('DOMContentLoaded', function() {
+        const MODAL = document.getElementById("modalWindow");
+        const BTNS = document.querySelectorAll(".getStarted");
+        const SPAN = document.getElementsByClassName("closeModalWindow")[0];
+        const OK = document.querySelector('#modalWindow form input[type="button"]');
+        const EMAIL = document.querySelector('#modalWindow form [name="email"]');
+        const PASSWORD = document.querySelector('#modalWindow form [name="password"]');
+        const ADDCOMMENT = getElementById('addComment');
 
-    BTNS.forEach(BTN => {
-        BTN.addEventListener('click', function() {
-            MODAL.style.display = "block";
-        })
-    });
-    SPAN.addEventListener('click', function() {
-        MODAL.style.display = "none";
-    });
-    
-
-    OK.addEventListener('click', function() {
-        const EMAIL_VALUE = EMAIL.value;
-        const PASSWORD_VALUE = PASSWORD.value;
-
-        if ((EMAIL_VALUE === ADMIN.email) && (PASSWORD_VALUE === ADMIN.password)) {
+        BTNS.forEach(BTN => {
+            BTN.addEventListener('click', function() {
+                MODAL.style.display = "block";
+            })
+        });
+        SPAN.addEventListener('click', function() {
             MODAL.style.display = "none";
-            location.href = "/articles.html";
-            return;
-        }
+        });
 
-        alert('Email or password is wrong')
+
+        OK.addEventListener('click', function() {
+            const EMAIL_VALUE = EMAIL.value;
+            const PASSWORD_VALUE = PASSWORD.value;
+
+            if ((EMAIL_VALUE === ADMIN.email) && (PASSWORD_VALUE === ADMIN.password)) {
+                MODAL.style.display = "none";
+                location.href = "/articles.html";
+                return;
+            }
+
+            alert('Email or password is wrong')
+        });
+        ADDCOMMENT.addEventListener('click', newComment);
     });
-    
-});
 }
 
-if (document.location.pathname=== '/create-article.html') {
-document.querySelector('#main-button').addEventListener('click',(event)=>{
-const author = document.getElementById('psevdo').value;
-const category = document.getElementById('cat').value;
-const title = document.getElementById('title').value;
-const text = document.getElementById('text').value;
-const img = 'http://lorempixel.com/640/480/business';
-console.log(author,category,title,text,img);
-service.addArticles(author,category,title,text,img);
-});
+function newComment(parentElement) {
+    if (!newCommentText) {
+        alert('Your message is empty.');
+        return
+    }
+    let indexOfNextElement = ++parentElement.children[parentElement.children.length - 1].id.split('CommentBlock')[1];
+
+    parentElement.innerHTML += `<div id="CommentBlock${indexOfNextElement}"><hr><p class="AuthorBlogContainer"><img src="img/default-avatar.jpg" class="Avatar"/> 
+    <b class="NickName CommentBox">${LOCALSTORAGE_KEY_NAME}</b><p>
+    <p class="CRUDBox">
+        <div class="CRUDElement" onclick="editComment('CommentText${indexOfNextElement}')">📝</div> 
+
+        <div class="CRUDElement" onclick="removeComment('CommentBlock${indexOfNextElement}')">❌</div>
+    </p>
+    <p class="CommentBox" id="CommentText${indexOfNextElement}">${newCommentText}</p>
+    <p class="CommentBox">👍 ${0}</p></div>`;
 }
-if (document.location.pathname=== '/articles.html') {
-    document.querySelector('.main-content-articles').addEventListener('click',(event)=>{
-    if (event.target.className === 'articles__item-text-header'){
-    console.log(event.target.id);
-    addArticleToLS(event.target.id);
-    location.href = "/article.html";
+
+
+
+if (document.location.pathname === '/create-article.html') {
+    document.querySelector('#main-button').addEventListener('click', (event) => {
+        const author = document.getElementById('psevdo').value;
+        const category = document.getElementById('cat').value;
+        const title = document.getElementById('title').value;
+        const text = document.getElementById('text').value;
+        const img = 'http://lorempixel.com/640/480/business';
+        console.log(author, category, title, text, img);
+        service.addArticles(author, category, title, text, img);
+    });
 }
-});
+if (document.location.pathname === '/articles.html') {
+    document.querySelector('.main-content-articles').addEventListener('click', (event) => {
+        if (event.target.className === 'articles__item-text-header') {
+            console.log(event.target.id);
+            addArticleToLS(event.target.id);
+            location.href = "/article.html";
+        }
+    });
 }
 if (document.location.pathname === '/article.html') {
     renderArticle();
     otherArticles();
-    document.querySelector('.edit-article').addEventListener('click', ()=>{
+    document.querySelector('.edit-article').addEventListener('click', () => {
         location.href = "/edit.html";
     });
-    
+
 }
 if (document.location.pathname === '/edit.html') {
-    renderEditArticle();   
-    document.querySelector('#main-button').addEventListener('click',(event)=>{
+    renderEditArticle();
+    document.querySelector('#main-button').addEventListener('click', (event) => {
         event.preventDefault();
         const author = document.getElementById('psevdo').value;
         const category = document.getElementById('cat').value;
         const title = document.getElementById('title').value;
         const text = document.getElementById('text').value;
         const img = 'http://lorempixel.com/640/480/business';
-        console.log(author,category,title,text,img);
+        console.log(author, category, title, text, img);
         const article = JSON.parse(localStorage.getItem('article'));
-        service.editArticle(article[0].id,author,category,title,text,img);
+        service.editArticle(article[0].id, author, category, title, text, img);
         location.href = "/articles.html";
-        });
+    });
 }
 
 
@@ -118,7 +138,7 @@ class Menu {
     }
 
     like(e) {
-        (e.className==="fas fa-heart")? e.className = "far fa-heart" : e.className = "fas fa-heart"
+        (e.className === "fas fa-heart") ? e.className = "far fa-heart": e.className = "fas fa-heart"
         const articleItemText = e.closest(".articles__item-text");
         const articleID = articleItemText.querySelector(".articles__item-text-header").id
         service.likeArticle(articleID)
@@ -138,10 +158,10 @@ class Menu {
     };
 
 }
-if(window.location.href.indexOf("articles.html") > -1){
+if (window.location.href.indexOf("articles.html") > -1) {
     const articles = document.querySelector(".main-content-articles")
     new Menu(articles);
-}else if(window.location.href.indexOf("article.html")> -1){
+} else if (window.location.href.indexOf("article.html") > -1) {
     const articles = document.querySelector(".other-articles")
     new Menu(articles);
 }
